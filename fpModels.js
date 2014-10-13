c17934186ef39ad4c6e3301ba36522c58863f501
naveenshankar <naveenshankarcse@gmail.com>
2014-10-13 06:38:52 -0700

    Update fpModels.js

--- fpModels.js
+++ fpModels.js
@@ -5,7 +5,7 @@ var fpModels = function(){
 		        			"userName":"Enter your username",
 		        			"password":'Enter your password',
 		        			"credentialStatus":false,
-		        			"videosApiKey":'AIzaSyB01qMKPma7RpTk-e-V893ioTRvjTIwLiU'
+		        			"videosApiKey":''
 		        			}
 
 		    });
